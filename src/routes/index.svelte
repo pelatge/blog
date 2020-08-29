fd618aac859875e15c5a22b12311966eab70b837
Nusendra Hanggarawan <admin@nusendra.com>
2020-08-29 17:42:41 +0700

    ui: home blog list

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,14 @@
 	<title>REVAMP</title>
 </svelte:head>
 
+<div class="container w-full md:max-w-3xl mx-auto pt-10 pb-10">
+  <div class="w-full px-4 md:px-6 text-gray-800 leading-normal">
+    <div class="text-sm text-gray-700 pb-5 font-mono">Recently Published</div>
+    <div class="text-2xl text-gray-700 font-bold">Judul blog</div>
+    <div class="text-1xl text-gray-700 font-medium">short description</div>
+    <div class="text-xs text-gray-700 font-medium">datetime</div>
+  </div>
+</div>
 
 <script lang="typescript">
   let a: number = 20;
