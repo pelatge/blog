9c94388eb3dee4c00b8cb80530de22e71f68d5b5
Nusendra Hanggarawan <admin@nusendra.com>
2020-08-29 20:18:41 +0700

    feat: add posts list

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,13 +1,3 @@
-<script context="module">
-import client, { defaultRequestConfig as reqConfig } from '../storyblokClient'
-
-export async function preload(page, session) {
-  const response = await client.getAll('cdn/stories', reqConfig)
-
-  return { stories: response || [] }
-}
-</script>
-
 <main>
   <body class="bg-gray-200 font-sans leading-normal tracking-normal">
     <Nav />
@@ -19,7 +9,4 @@ export async function preload(page, session) {
 <script>
 import Nav from '../components/Nav.svelte';
 import Footer from '../components/Footer.svelte';
-
-export let stories
-  console.log(stories)
 </script>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,13 @@
+<script context="module">
+import client, { defaultRequestConfig as reqConfig } from '../storyblokClient'
+
+export async function preload(page, session) {
+  const response = await client.getAll('cdn/stories', reqConfig)
+
+  return { stories: response || [] }
+}
+</script>
+
 <svelte:head>
 	<title>Nusendra.com | Home</title>
 </svelte:head>
@@ -5,14 +15,15 @@
 <div class="container w-full md:max-w-3xl mx-auto pt-10 pb-10">
   <div class="w-full px-4 md:px-6 text-gray-800 leading-normal">
     <div class="text-sm text-gray-700 pb-5 font-mono">Recently Published</div>
-    <div class="text-2xl text-gray-700 font-bold">Judul blog</div>
-    <div class="text-1xl text-gray-700 font-medium">short description</div>
-    <div class="text-xs text-gray-700 font-medium">datetime</div>
+    {#each stories as story}
+      <div class="text-2xl text-gray-700 font-bold">{story.content.title}</div>
+      <div class="text-1xl text-gray-700 font-medium">{story.content.intro}</div>
+      <div class="text-xs text-gray-700 font-medium">{story.first_published_at}</div>
+    {/each}
   </div>
 </div>
 
 <script lang="typescript">
-  let stories: Array<any> = [];
-
-  console.log(stories);
+  export let stories: Array<any>;
+  console.log(stories)
 </script>
