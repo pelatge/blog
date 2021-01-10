74139eae127231bab961ca6e022aab08ae22e66b
nusendra <admin@nusendra.com>
2021-01-10 16:59:39 +0700

    feat: separate nav and layout

---
+++ src/components/Nav.svelte
@@ -0,0 +1,46 @@
+<div class="relative bg-white">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6">
+    <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+      <div class="flex justify-start lg:w-0 lg:flex-1">
+        <a href="#">
+          <span class="sr-only">Workflow</span>
+          <img class="h-8 w-auto sm:h-10" src="/images/logo.png" alt="">
+        </a>
+      </div>
+      <div class="-mr-2 -my-2 md:hidden">
+        <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+          <span class="sr-only">Open menu</span>
+          <!-- Heroicon name: menu -->
+          <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+      </div>
+      <nav class="hidden md:flex space-x-10">
+        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+         Home
+        </a>
+        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+          Articles
+        </a>
+        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+          Free Courses
+        </a>
+        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+          Now
+        </a>
+        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+          Contact
+        </a>
+        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+          About
+        </a>
+      </nav>
+      <!-- <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0"> -->
+      <!--   <a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> -->
+      <!--     Premium Courses -->
+      <!--   </a> -->
+      <!-- </div> -->
+    </div>
+  </div>
+</div>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,185 +1,90 @@
 <script>
-  import GoogleAnalytics from "../components/GoogleAnalytics.svelte";
+    import GoogleAnalytics from "../components/GoogleAnalytics.svelte";
+    import Nav from "../components/Nav.svelte";
 </script>
 
 <GoogleAnalytics />
 
-<div class="relative bg-white">
-  <div class="max-w-7xl mx-auto px-4 sm:px-6">
-    <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-      <div class="flex justify-start lg:w-0 lg:flex-1">
-        <a href="#">
-          <span class="sr-only">Workflow</span>
-          <img class="h-8 w-auto sm:h-10" src="/images/logo.png" alt="">
-        </a>
-      </div>
-      <div class="-mr-2 -my-2 md:hidden">
-        <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-          <span class="sr-only">Open menu</span>
-          <!-- Heroicon name: menu -->
-          <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-        </button>
-      </div>
-      <nav class="hidden md:flex space-x-10">
-        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
-         Home
-        </a>
-        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
-          Articles
-        </a>
-        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
-          Free Courses
-        </a>
-        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
-          Now
-        </a>
-        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
-          Contact
-        </a>
-        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
-          About
-        </a>
-      </nav>
-      <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-        <a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-          Premium Courses
-        </a>
-      </div>
+<Nav />
+
+<div class="bg-gray-200 overflow-x-hidden">
+    <div class="px-2 py-8">
+        <div class="flex justify-between container mx-auto">
+            <div class="mx-8 w-12/12 hidden lg:block">
+                <div class="px-2">
+                    <div class="flex flex-col bg-white max-w-sm px-6 py-4 mx-auto rounded-lg shadow-md">
+                        <figure class="bg-gray-100 rounded-xl p-8">
+                          <img class="w-36 h-36 rounded-full mx-auto" src="/images/profile-photo.jpg" alt="" width="384" height="512">
+                          <div class="pt-6 space-y-4 font-medium text-center">
+                            <blockquote>
+                              <p class="text-lg font-semibold">
+                                “Conveys even one function”
+                              </p>
+                            </blockquote>
+                            <figcaption class="font-medium">
+                              <div class="text-cyan-600">
+                                  Nusendra H.
+                              </div>
+                              <div class="text-gray-500">
+                                  Software Developer
+                              </div>
+                            </figcaption>
+                          </div>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full lg:w-12/12">
+                <div>
+                    <div class="max-w-8xl px-10 py-6 bg-white rounded-lg shadow-md">
+                        <div class="flex justify-between items-center"><span class="font-light text-gray-600">Jun 1,
+                                2020</span><a href="#"
+                                class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">Laravel</a>
+                        </div>
+                        <div class="mt-2"><a href="#" class="text-2xl text-gray-700 font-bold hover:underline">Build
+                                Your New Idea with Laravel Freamwork.</a>
+                            <p class="mt-2 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                                Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim
+                                reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!</p>
+                        </div>
+                        <div class="flex justify-between items-center mt-4"><a href="#"
+                                class="text-blue-500 hover:underline">Read more</a>
+                            <div><a href="#" class="flex items-center"><img
+                                        src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=731&amp;q=80"
+                                        alt="avatar" class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block">
+                                    <h1 class="text-gray-700 font-bold hover:underline">Alex John</h1>
+                                </a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <div class="flex">
+                        <a href="#" class="mx-1 px-3 py-2 bg-white text-gray-500 font-medium rounded-md cursor-not-allowed">
+                            previous
+                        </a>
+                    
+                        <a href="#" class="mx-1 px-3 py-2 bg-white text-gray-700 font-medium hover:bg-blue-500 hover:text-white rounded-md">
+                            1
+                        </a>
+                    
+                        <a href="#" class="mx-1 px-3 py-2 bg-white text-gray-700 font-medium hover:bg-blue-500 hover:text-white rounded-md">
+                            2
+                        </a>
+                    
+                        <a href="#" class="mx-1 px-3 py-2 bg-white text-gray-700 font-medium hover:bg-blue-500 hover:text-white rounded-md">
+                            3
+                        </a>
+                    
+                        <a href="#" class="mx-1 px-3 py-2 bg-white text-gray-700 font-medium hover:bg-blue-500 hover:text-white rounded-md">
+                            Next
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-
-  <!--
-    Mobile menu, show/hide based on mobile menu state.
-
-    Entering: "duration-200 ease-out"
-      From: "opacity-0 scale-95"
-      To: "opacity-100 scale-100"
-    Leaving: "duration-100 ease-in"
-      From: "opacity-100 scale-100"
-      To: "opacity-0 scale-95"
-  -->
-  <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-    <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-      <div class="pt-5 pb-6 px-5">
-        <div class="flex items-center justify-between">
-          <div>
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
-          </div>
-          <div class="-mr-2">
-            <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-              <span class="sr-only">Close menu</span>
-              <!-- Heroicon name: x -->
-              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </button>
-          </div>
-        </div>
-        <div class="mt-6">
-          <nav class="grid gap-y-8">
-            <a href="#" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-              <!-- Heroicon name: chart-bar -->
-              <svg class="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-              </svg>
-              <span class="ml-3 text-base font-medium text-gray-900">
-                Analytics
-              </span>
-            </a>
-
-            <a href="#" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-              <!-- Heroicon name: cursor-click -->
-              <svg class="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
-              </svg>
-              <span class="ml-3 text-base font-medium text-gray-900">
-                Engagement
-              </span>
-            </a>
-
-            <a href="#" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-              <!-- Heroicon name: shield-check -->
-              <svg class="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-              </svg>
-              <span class="ml-3 text-base font-medium text-gray-900">
-                Security
-              </span>
-            </a>
-
-            <a href="#" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-              <!-- Heroicon name: view-grid -->
-              <svg class="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-              </svg>
-              <span class="ml-3 text-base font-medium text-gray-900">
-                Integrations
-              </span>
-            </a>
-
-            <a href="#" class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-              <!-- Heroicon name: refresh -->
-              <svg class="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
-              </svg>
-              <span class="ml-3 text-base font-medium text-gray-900">
-                Automations
-              </span>
-            </a>
-          </nav>
-        </div>
-      </div>
-      <div class="py-6 px-5 space-y-6">
-        <div class="grid grid-cols-2 gap-y-4 gap-x-8">
-          <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">
-            Pricing
-          </a>
-
-          <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">
-            Docs
-          </a>
-
-          <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">
-            Enterprise
-          </a>
-
-          <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">
-            Blog
-          </a>
-
-          <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">
-            Help Center
-          </a>
-
-          <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">
-            Guides
-          </a>
-
-          <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">
-            Security
-          </a>
-
-          <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">
-            Events
-          </a>
-        </div>
-        <div>
-          <a href="#" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-            Sign up
-          </a>
-          <p class="mt-6 text-center text-base font-medium text-gray-500">
-            Existing customer?
-            <a href="#" class="text-indigo-600 hover:text-indigo-500">
-              Sign in
-            </a>
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
 </div>
+
 
 
 <div>
